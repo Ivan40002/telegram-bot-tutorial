88238025da4bae35f9bd003bd366d41f20867e39
Ivan <packolinivan@gmail.com>
2024-04-25 17:26:30 +0300

    remove token

--- showroombot/config.py
+++ showroombot/config.py
@@ -1,6 +1,6 @@
 import base64
 
-bot_secret = '6707614015:AAFZ-EX1hh1yvWc6jK-fgwMuW6onrJfXsJE'
+bot_secret = ''
 
 #TELEGRAM_BOT_TOKEN = base64.b64decode(bot_secret).decode()
 TELEGRAM_BOT_TOKEN = bot_secret
